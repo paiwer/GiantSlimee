a7e83a914cb058fd4b8f6baf85d93edd9beab8a8
Pitchaporn Boonyakongrat <91454193+paiwer@users.noreply.github.com>
2023-10-28 15:10:39 +0700

    Add feature : UI hp bar

---
+++ Assets/Scripts/UIHpBar.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHpBar : MonoBehaviour
+{
+    [SerializeField] private float _maxHealth;
+    [SerializeField] private float _currentHealth;
+
+    [SerializeField] private float _animSpeed;
+    [SerializeField] private float _animDelay;
+
+    [SerializeField] private Image _hpBar;
+    [SerializeField] private Image _healBar;
+    [SerializeField] private Image _damageBar;
+
+    private PlayerInfo _playerInfo;
+
+    private float _currentFillAmount;
+    private float timer;
+
+    private float _previousHp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerInfo = FindObjectOfType<PlayerInfo>();
+
+        _maxHealth = _playerInfo.MaxHP;
+
+        _currentFillAmount = _currentHealth / _maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentHealth = _playerInfo.CurrentHP;
+
+        if (_currentHealth != _previousHp) //Hp change
+        {
+            if(_currentHealth > _previousHp)    //Increase
+            {
+                FillAmountCalculate();
+
+                SetAnimTimer();
+                SetTakeDamageFalse();
+
+                HPBarIncrease();
+            }
+
+            if(_currentHealth < _previousHp)    //Reduce
+            {
+                FillAmountCalculate();
+
+                SetAnimTimer();
+                SetTakeDamageFalse();
+
+                HPBarDecrease();
+            }
+        }
+
+        HealthBarAnim();
+    }
+
+    private void FillAmountCalculate()
+    {
+        _currentFillAmount = _currentHealth / _maxHealth;
+    }
+
+    private void SetAnimTimer()
+    {
+        timer = Time.time + _animDelay;
+    }
+
+    private void SetTakeDamageFalse()
+    {
+        _previousHp = _currentHealth;
+    }
+
+    private void HPBarIncrease()
+    {
+        if (_healBar.fillAmount == _damageBar.fillAmount)    //Normal increase
+        {
+            _healBar.fillAmount = _currentFillAmount;
+            _damageBar.fillAmount = _currentFillAmount;
+        }
+        if (_hpBar.fillAmount != _damageBar.fillAmount)    //Increase While take damage or Increase then take damage
+        {
+            _healBar.fillAmount = _currentFillAmount;
+        }
+    }
+
+    private void HPBarDecrease()
+    {
+        if (_hpBar.fillAmount == _healBar.fillAmount)    //Normal decrease
+        {
+            _hpBar.fillAmount = _currentFillAmount;
+            _healBar.fillAmount = _currentFillAmount;
+        }
+        if (_hpBar.fillAmount != _damageBar.fillAmount)  //Decrease While heal
+        {
+            _healBar.fillAmount = _currentFillAmount;
+        }
+    }
+
+    private void HealthBarAnim()
+    {
+        if (Time.time > timer)
+        {
+            if (_hpBar.fillAmount != _currentFillAmount)
+            {
+                _hpBar.fillAmount = Mathf.MoveTowards(_hpBar.fillAmount, _currentFillAmount, _animSpeed * Time.deltaTime);
+            }
+            if (_healBar.fillAmount != _currentFillAmount)
+            {
+                _healBar.fillAmount = Mathf.MoveTowards(_healBar.fillAmount, _currentFillAmount, _animSpeed * Time.deltaTime);
+            }
+            if (_damageBar.fillAmount != _currentFillAmount)
+            {
+                _damageBar.fillAmount = Mathf.MoveTowards(_damageBar.fillAmount, _currentFillAmount, _animSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
